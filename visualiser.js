7a30d6e22de548aed221db964b86d5706c7d181a
Samuel Fang <samuelfangjw@gmail.com>
2020-05-28 03:42:52 +0800

    added bar graph and pie chart

--- visualiser.js
+++ visualiser.js
@@ -1,10 +1,47 @@
-google.charts.load('current', {'packages':['timeline']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart','timeline']});
+google.charts.setOnLoadCallback(drawTimeline);
+google.charts.setOnLoadCallback(drawPieChart);
 
 const array = [['Activity', 'Start Time', 'End Time']];
+const pie_array = [['Task', 'Hours per Day']];
+const bar_array = [['Task', 'Hours per Day', {role: 'style'}]];
 const raw_array = JSON.parse(localStorage.getItem("array"));
 const length = raw_array.length;
+const colors = [
+  "#3366cc",
+  "#dc3912",
+  "#ff9900",
+  "#109618",
+  "#990099",
+  "#0099c6",
+  "#dd4477",
+  "#66aa00",
+  "#b82e2e",
+  "#316395",
+  "#994499",
+  "#22aa99",
+  "#aaaa11",
+  "#6633cc",
+  "#e67300",
+  "#8b0707",
+  "#651067",
+  "#329262",
+  "#5574a6",
+  "#3b3eac",
+  "#b77322",
+  "#16d620",
+  "#b91383",
+  "#f4359e",
+  "#9c5935",
+  "#a9c413",
+  "#2a778d",
+  "#668d1c",
+  "#bea413",
+  "#0c5922",
+  "#743411",
+]
 
+//generating arrays from string data
 for(let i = 0; i < length; i++) {
   const temp_arr = raw_array[i];
   const activity = temp_arr[0];
@@ -12,12 +49,31 @@ for(let i = 0; i < length; i++) {
   const minutes1 = temp_arr[2];
   const hours2 = temp_arr[3];
   const minutes2 = temp_arr[4];
-  const datefrom = new Date(2020, 01, 01, hours1, minutes1);
-  const dateto = new Date(2020, 01, 01, hours2, minutes2);
+  let datefrom = new Date(0, 0, 1, hours1, minutes1);
+  let dateto = new Date(0, 0, 1, hours2, minutes2);
+  if (hours1 > hours2 || (hours1 == hours2 && minutes1 > minutes2)) {
+    datefrom = new Date(0, 0, 0, hours1, minutes1);
+    dateto = new Date(0, 0, 1, hours2, minutes2);
+  }
   array.push([activity,datefrom,dateto]);
 }
 
-function drawChart() {
+for(let i = 1; i <= length; i++) {
+  const temp_arr = array[i];
+  const activity = temp_arr[0];
+  const time_spent = Math.abs(temp_arr[1] - temp_arr[2]) / 36e5;
+  pie_array.push([activity, time_spent]);
+}
+
+for (let i = 1; i <= length; i++){ 
+  const temp_arr = pie_array[i];
+  const activity = temp_arr[0];
+  const time_spent = temp_arr[1];
+  const color = colors[((i-1) % 32)];
+  bar_array.push([activity, time_spent, color]);
+}
+
+function drawTimeline() {
   var data = google.visualization.arrayToDataTable(array);
 
   var options = {
@@ -27,4 +83,27 @@ function drawChart() {
   var chart = new google.visualization.Timeline(document.getElementById('chart_div'));
 
   chart.draw(data, options);
+}
+
+function drawPieChart() {
+  var data = google.visualization.arrayToDataTable(pie_array);
+  var bardata = google.visualization.arrayToDataTable(bar_array);
+
+  var pieoptions = {
+    title: 'Pie Chart',
+    height: 300,
+    width: 500,
+  };
+
+  var baroptions = {
+    title: 'Bar Chart',
+    width: 700,
+    height: 300,
+  };
+
+  var piechart = new google.visualization.PieChart(document.getElementById('piechart'));
+  var barchart = new google.visualization.ColumnChart(document.getElementById('barchart'));
+
+  piechart.draw(data, pieoptions);
+  barchart.draw(bardata, baroptions);
 }
